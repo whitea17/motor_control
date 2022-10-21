d217d728dc66868b7f91871070f6b592bc4c6cfb
Austin White <whitea17@spu.edu>
2022-10-20 22:00:46 -0700

    speed: Implement speed control via PWM of enable pin
    Additionally, change pin map mode from GPIO.BCM to GPIO.BOARD.
    GPIO.BOARD now matches pin numbers printed on board and allows
    for pwm.

--- basic_move.py
+++ basic_move.py
@@ -5,39 +5,39 @@ car_dir = 0  # f = 1, b = 2, fr = 3, fl = 4, br = 5, bl = 6
 
 # GPIO pins
 motor_pin_1 = 12
-motor_pin_2 = 18
-enable_pin = 13
+motor_pin_2 = 32
+enable_pin = 33
 
 #GPIO states
 motor_pin_1_state = 0
 motor_pin_2_state = 0
-enable_pin_state = 0
-
+enable_pin_pwm_val = 0
 
 def forward(speed):
   print("Going forward")
   global motor_pin_1_state
   global motor_pin_2_state
   global enable_pin_state
+  global enable_pin_pwm_val
 
   motor_pin_1_state = 1
   motor_pin_2_state = 0
-  enable_pin_state = 1
-
+  enable_pin_pwm_val = speed
 
 def backward(speed):
-  print("Going backward at warp " + speed)
+  print("Going backward at warp " + str(speed))
   global motor_pin_1_state
   global motor_pin_2_state
   global enable_pin_state
+  global enable_pin_pwm_val
 
   motor_pin_1_state = 0
   motor_pin_2_state = 1
-  enable_pin_state = 1
+  enable_pin_pwm_val = speed
 
 
 def forward_right(speed):
-  print("Going forward right at warp " + speed)
+  print("Going forward right at warp " + str(speed))
 
 
 def forward_left(speed):
@@ -55,13 +55,16 @@ def backward_left(speed):
 def main():
   global motor_pin_1_state
   global motor_pin_2_state
-  global enable_pin_state
+  global enable_pin_pwm_val
 
   # Setup
-  GPIO.setmode(GPIO.BCM)
+  GPIO.setmode(GPIO.BOARD)
   GPIO.setup(motor_pin_1, GPIO.OUT)
   GPIO.setup(motor_pin_2, GPIO.OUT)
-  GPIO.setup(enable_pin, GPIO.OUT)
+  GPIO.setup(enable_pin, GPIO.OUT, initial=GPIO.HIGH)
+  enable_pin_pwm = GPIO.PWM(enable_pin, 50)
+  enable_pin_pwm.start(0)
+
 
   # Loop
   while True:
@@ -70,10 +73,10 @@ def main():
     user_input_split = user_input.split(",")
 
     dir = int(user_input_split[0])
-    s = user_input_split[1]
+    s = int(user_input_split[1])
     t = int(user_input_split[2])
 
-
+    # Interpert command and adjust vars
     if (dir == 0):
       time.sleep(t)
       continue
@@ -91,18 +94,22 @@ def main():
     elif (dir == 6):
       backward_left(s)
 
-    print(motor_pin_1_state)
+    print(enable_pin_pwm_val)
+    # Execute current loop's command based on var states
     GPIO.output(motor_pin_1, motor_pin_1_state)
     GPIO.output(motor_pin_2, motor_pin_2_state)
-    GPIO.output(enable_pin, enable_pin_state)
-
+    enable_pin_pwm.ChangeDutyCycle(enable_pin_pwm_val)
     time.sleep(t)
+
+    # Reset vars and GPIO states
     motor_pin_1_state = 0
     motor_pin2_state = 0
     enable_pin_state = 0
+    enable_pin_pwm_val = 0
     GPIO.output(motor_pin_1, 0)
     GPIO.output(motor_pin_2, 0)
-    GPIO.output(enable_pin, 0)
+    enable_pin_pwm.ChangeDutyCycle(enable_pin_pwm_val)
+
 
 main()
 
